55538b520f81ebb71d413aa3acb5802f7c2ad978
Ellie Boyd <ellie.r.boyd@gmail.com>
2023-05-15 21:47:13 -0400

    attempt 3 to fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,11 @@ import autoprefixer from 'autoprefixer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['./fortawesome/fontawesome-svg-core'],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['./fortawesome/fontawesome-svg-core'],
+  //   },
+  // },
   plugins: [
     eslint(),
   ],
